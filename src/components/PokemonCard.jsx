8ac42fc7b6d0955b3a07486d88f32c970a30c617
Gwenael <gwenael.begot@outlook.fr>
2023-12-12 11:52:20 +0100

    add mew ???

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,27 @@
 
+
 function PokemonCard() {
+  const pokemon = pokemonList[1];
     return (
       <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+        {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
+  
         <figcaption>
-            <p>Bulbasaur</p>
+        <p>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
         </figcaption>
       </figure>
     );
   }
   
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   export default PokemonCard;
