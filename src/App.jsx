d8a83264e94b90c53caab8b2195220d3c0e9f30d
Gwenael <gwenael.begot@outlook.fr>
2023-12-13 16:12:40 +0100

    add state + btn

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 
+
 function App() {
+
+const [pokemonIndex, setPokemonIndex] = useState(0)
 
   const pokemonList = [
     {
@@ -11,6 +14,36 @@ function App() {
       nDex : "001",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "Ivysaur",
+      nDex : "002",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+    },
+    {
+      name: "Venusaurr",
+      nDex : "003",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+    },
+    {
+      name: "Charmander",
+      nDex : "004",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "Charmeleon",
+      nDex : "005",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
+    },
+    {
+      name: "Charizard",
+      nDex : "006",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
     },
     {
       name: "mew",
@@ -24,10 +57,20 @@ function App() {
     },
   ];
 
+  const handlePrevious = () => {
+    setPokemonIndex((prevState) => (prevState > 0 ? prevState -1 : 0));
+  };
+
+  const handleNext = () => {
+    setPokemonIndex ((prevState) => (prevState < pokemonList.length - 1 ? prevState +1 : index));
+  };
+
   return (
     <>
       <div>
-        <PokemonCard pokemon={pokemonList[0]}/>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+        {pokemonIndex > 0 && <button onClick={handlePrevious}>Précédent</button>}
+        {pokemonIndex < pokemonList.length - 1 && <button onClick={handleNext}>Suivant</button>}
       </div>
     </>
   )
